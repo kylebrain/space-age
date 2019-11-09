a62438fbaaba1cd05bb23bd5e60160bb8dd321c3
Garrett Moore <garrett22smoore@gmail.com>
2019-11-09 00:33:43 -0800

    garrett's initial commit

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -32,6 +32,7 @@ public class Player : MonoBehaviour
             {
                 GetComponent<Rigidbody>().velocity += new Vector3(0, jumpHeight, 0);
                 Debug.Log("kyle gae");
+                Debug.Log("kyle IS gae");
             }
             jumped = false;
         }
