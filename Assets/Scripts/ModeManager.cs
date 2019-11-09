67638ad4b363feedc6efc77cccd990040b41265c
Eryl Kenner <eryl.kenner@gmail.com>
2019-11-09 14:41:28 -0800

    Mode switching class now changes sprite transparency

---
+++ Assets/Scripts/ModeManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModeManager : MonoBehaviour
+{
+
+    public void ModeSwitch(Player player, PlayerMode playerMode)
+    {
+        SpriteRenderer sprite = player.GetComponent<SpriteRenderer>();
+        switch (playerMode)
+        {
+            case PlayerMode.GunnerMode:
+                sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, 0.3f);
+                break;
+            case PlayerMode.PilotMode:
+                sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, 0.3f);
+                break;
+            case PlayerMode.CabinMode:
+                sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, 1.0f);
+                break;
+            default:
+                break;
+        }
+    }
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+}
