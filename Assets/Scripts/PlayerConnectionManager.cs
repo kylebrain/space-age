d3971d201230c65db9972d935f68ad133830cead
Kyle-Laptop <kylebrain@gmail.com>
2019-11-10 09:55:57 -0800

    Added GameStart screen which disapeers on player connect, added game over screen

--- Assets/Scripts/PlayerConnectionManager.cs
+++ Assets/Scripts/PlayerConnectionManager.cs
@@ -9,10 +9,13 @@ public class PlayerConnectionManager : MonoBehaviour
     public Sprite blueMan;
     public Sprite redMan;
 
+    public GameObject startScreen;
+
     private bool lastBlueMan = false;
 
     public void OnPlayerJoined(PlayerInput playerInput)
     {
+        startScreen.SetActive(false);
         playerInput.GetComponent<SpriteRenderer>().sprite = lastBlueMan ? redMan : blueMan;
         lastBlueMan = !lastBlueMan;
     }
