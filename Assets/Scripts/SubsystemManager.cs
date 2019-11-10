e130c23c3b91b30d51e69df85c9bbe1e9b513792
Michael Martinelli <martinellimichael15@gmail.com>
2019-11-09 20:32:04 -0800

    Touched up repair system

--- Assets/Scripts/SubsystemManager.cs
+++ Assets/Scripts/SubsystemManager.cs
@@ -5,9 +5,9 @@ using UnityEngine.UI;
 
 public static class SubsystemManager
 {
-    public static int shieldHealth = 100;
-    public static int weaponHealth = 100;
-    public static int navigationHealth = 100;
+    public static int shieldHealth = 69;
+    public static int weaponHealth = 69;
+    public static int navigationHealth = 69;
     public static int mainHealth = 200;
     
 
