db5d02d12d90fe53181e7aeed347fc930e2f5465
Kyle-Laptop <kylebrain@gmail.com>
2019-11-10 11:18:50 -0800

    Added scoring system

---
+++ Assets/Scripts/EndScreenUpdate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndScreenUpdate : MonoBehaviour
+{
+    public Text scoreText;
+
+
+    private void OnEnable()
+    {
+        scoreText.text = "Score: " + SubsystemManager.score.ToString();
+    }
+}
