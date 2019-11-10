c8e03ff3ffa6f79d044d6a64148ec8eae1129c7e
Eryl Kenner <eryl.kenner@gmail.com>
2019-11-10 04:16:10 -0800

    Enemies must lock on to pilor before shooting

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Laser laser;
+    public Transform gunPosition;
+    public float speed;
+    public float stoppingDistance;
+    public float shootingDistance;
+
+    float turnRate;
+    float fireRate;
+    float fireAccumulator;
+
+    void Start()
+    {
+        speed = 10.0f;
+        stoppingDistance = 20f;
+        shootingDistance = 30f;
+        fireRate = 1.2f;
+        fireAccumulator = 0.0f;
+        turnRate = 90f;
+    }
+
+    void FixedUpdate()
+    {
+        float angleDiff = Vector3.SignedAngle(transform.up, Pilot.instance.transform.position - transform.position, Vector3.up);
+        if (Mathf.Abs(angleDiff) <= turnRate * Time.fixedDeltaTime)
+        {
+            transform.Rotate(new Vector3(0, angleDiff, 0), Space.World);
+        }
+        else if (angleDiff > 0)
+        {
+            transform.Rotate(new Vector3(0, turnRate * Time.fixedDeltaTime, 0), Space.World);
+        }
+        else
+        {
+            transform.Rotate(new Vector3(0, -turnRate * Time.fixedDeltaTime, 0), Space.World);
+        }
+
+
+        float dist = Vector3.Distance(transform.position, Pilot.instance.transform.position);
+        if (dist > stoppingDistance)
+        {
+            transform.localPosition += transform.up * speed * Time.fixedDeltaTime;
+        }
+
+        if (dist <= shootingDistance)
+        {
+            fireAccumulator += Time.fixedDeltaTime;
+            while (fireAccumulator >= 1f / fireRate)
+            {
+                fireAccumulator -= 1f / fireRate;
+                if (Mathf.Abs(angleDiff) <= 4)
+                {
+                    Instantiate(laser, gunPosition.position, Quaternion.LookRotation(transform.up, Vector3.up));
+                }
+            }
+        }
+        else
+        {
+            fireAccumulator = 0.0f;
+        }
+
+
+    }
+}
