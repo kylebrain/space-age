63eea18932ad9747bebebf8dbdd3c84306a2b530
Kyle-Laptop <kylebrain@gmail.com>
2019-11-10 09:36:25 -0800

    Color alternates for players spawned

---
+++ Assets/Scripts/PlayerConnectionManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(PlayerInputManager))]
+public class PlayerConnectionManager : MonoBehaviour
+{
+    public Sprite blueMan;
+    public Sprite redMan;
+
+    private bool lastBlueMan = false;
+
+    public void OnPlayerJoined(PlayerInput playerInput)
+    {
+        playerInput.GetComponent<SpriteRenderer>().sprite = lastBlueMan ? redMan : blueMan;
+        lastBlueMan = !lastBlueMan;
+    }
+}
